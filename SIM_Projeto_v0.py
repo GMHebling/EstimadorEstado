33bda5e9642e4a9a2fd26a073c63ca2b599a2702
Julio Massignan <juliomassignan@gmail.com>
2020-03-21 12:29:55 -0300

    SIM_Proj_py

---
+++ SIM_Projeto_v0.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+SIMULADOR MONTE CARLO - ESTIMADOR DE ESTADO TRIFÁSICO
+
+Script para análise estatística do estimador de estado trifásico por simulação de Monte Carlo
+
+1. Monta caso de referência com base em cálculo de fluxo de potência
+
+2. Simulação quasi-estacionária para avaliação temporal
+
+3. Emulação de planos de medição
+
+4. Amostragem de ruído aleatório e Simulação de Monte Carlo 
+
+Created on Sat Mar 21 12:05:29 2020
+
+@author: Julio Massignan
+"""
+
+
+
+#-----------------------------------------------------------------------------
+# LEITURA DE DADOS
+#
+#-----------------------------------------------------------------------------
+md = '/home/julio/projetos'
+sd = '/IEEE342/' 
+
+
+
+
+
+
+#-----------------------------------------------------------------------------
+# CASO DE REFERÊNCIA
+#
+#-----------------------------------------------------------------------------   
+
+
+
+
+#-----------------------------------------------------------------------------
+# SIMULAÇÃO DE MONTE CARLO
+#
+#-----------------------------------------------------------------------------
+
+# Montagem do plano de medição
+
+# Amostragem de ruído aleatório
+
+# Exporta medidas
+
